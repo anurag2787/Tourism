e38817adc4bb2bf7ce6c16345c4d8c668e26876e
anurag2787 <143180737+anurag2787@users.noreply.github.com>
2024-02-08 00:07:56 +0530

    Jaunpur Tourism
    A basis site using html css and a small use of java script

---
+++ style.js
@@ -0,0 +1,15 @@
+function sahi(){
+    window.open("https://en.wikipedia.org/wiki/Shahi_Bridge",'_blank');
+    console.log("done")
+}
+
+function atala(){
+    window.open("https://en.wikipedia.org/wiki/Atala_Mosque,_Jaunpur",'_blank');
+    console.log("done")
+}
+
+function quila(){
+    // window.location.href = "https://en.wikipedia.org/wiki/Shahi_Qila,_Jaunpur";
+    window.open("https://en.wikipedia.org/wiki/Shahi_Qila,_Jaunpur",'_blank');
+    console.log("done")
+}
